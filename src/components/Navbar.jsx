adac549392e00201d9dcae88a270521017f7732b
Lucia Seba <luciaseba0@gmail.com>
2022-03-18 08:50:43 -0300

    Menubar in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 
 import { TabMenu } from 'primereact/tabmenu';
 import { useState } from "react";
+import { Menubar } from 'primereact/menubar';
 
 const Navbar = ({index}) => {
     const [activeIndex, setActiveIndex] = useState(index); 
-
+    
     const items = [
-        { label: 'Home', icon: 'pi pi-fw pi-home', url: '/home' },
-        { label: 'Capacitaciones', icon: 'pi pi-fw pi-briefcase', url: '/training' },
-        { label: 'Accesos', icon: 'pi pi-fw pi-key', url: '/access' },
-        { label: 'Software', icon: 'pi pi-fw pi-database', url: '/software' },
+        {
+        label:'Home',
+        icon:'pi pi-fw pi-home',
+        url: '/home'
+        },
+        {
+        label:'Capacitaciones',
+        icon:'pi pi-fw pi-briefcase',
+        url: '/training'
+        },
+        {
+        label:'Accesos',
+        icon:'pi pi-fw pi-key',
+        url: '/access'
+        },
+        {
+        label:'Software',
+        icon:'pi pi-fw pi-database',
+        url: '/software'
+        
+        },
     ];
-
+    
     return (
-        <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.value)}  />
+        <Menubar model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.value)}/>
     );
 }
 export default Navbar;
